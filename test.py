21adc0ac47dfb65525af7ae7bf635b1cf794a8ce
Ajay <115636212+ajaygithub2@users.noreply.github.com>
2023-04-12 20:59:00 +0530

    Update test.py

--- test.py
+++ test.py
@@ -3,19 +3,19 @@ from streamlit_webrtc import webrtc_streamer, VideoTransformerBase, WebRtcMode
 
 
 # Define a custom VideoTransformer class to handle video frames
-class WebcamVideoTransformer(VideoTransformerBase):
-    def __init__(self):
-        self.width = None
-        self.height = None
+# class WebcamVideoTransformer(VideoTransformerBase):
+#     def __init__(self):
+#         self.width = None
+#         self.height = None
 
-    def transform(self, frame):
-        return frame
+#     def transform(self, frame):
+#         return frame
 
-    def on_receive(self, frame):
-        if self.width is None or self.height is None:
-            self.width = frame.width
-            self.height = frame.height
-        return frame
+#     def on_receive(self, frame):
+#         if self.width is None or self.height is None:
+#             self.width = frame.width
+#             self.height = frame.height
+#         return frame
 
 
 # Streamlit app
@@ -24,7 +24,7 @@ def main():
     st.write("Use the button below to start the webcam video feed.")
 
     # Display video feed using webrtc_streamer
-    webrtc_streamer(key="webcam", video_processor_factory=WebcamVideoTransformer, mode=WebRtcMode.SENDRECV)
+    webrtc_streamer(key="webcam")#, video_processor_factory=WebcamVideoTransformer, mode=WebRtcMode.SENDRECV)
 
 
 if __name__ == "__main__":
